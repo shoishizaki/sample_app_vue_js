86e88ceecfbe36d0fc3d27e563fe7a3fbdf9e6a9
EC2 Default User <ec2-user@ip-172-31-18-29.us-east-2.compute.internal>
2020-03-14 10:58:03 +0000

    Create bacic sign up form

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
+  <div class="signup">
     <h3>Sign Up</h3>
+    <label for="username">User Name</label>
+    <input id="username" type="text" v-model="userdata.name"><br>
+    <label for="useremail">User Email</label>
+    <input id="useremail" type="text" v-model.lazy="userdata.email"><br>
+    <label for="userpassword">Password</label>
+    <input id="userpassword" type="password" v-model.lazy="userdata.password">
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        userdata: {
+          name: "",
+          email: "",
+          password: ""
+        }
+      };
+    }
+  }
+</script>
+
+<style scoped>
+.signup{
+  text-align: center;
+}
+  
+</style>
