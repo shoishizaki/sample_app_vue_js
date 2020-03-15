8f6634a5d812e39f2663e0ff1b707af6f657d471
EC2 Default User <ec2-user@ip-172-31-18-29.us-east-2.compute.internal>
2020-03-15 07:48:01 +0000

    Create function to send post to server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <h1>Welcome to the sample App</h1>
-    <button @click="toSignUp">Sign up now!!</button>
-    <h3>This is the home page for the microposts application by <a href="https://jp.vuejs.org/index.html">vue.js</a> </h3>
-    
-    <img src="@/assets/logo.png"/>
+    <template>
+      <h1>Welcome to the sample App</h1>
+      <button @click="toSignUp">Sign up now!!</button>
+      <h3>This is the home page for the microposts application by <a href="https://jp.vuejs.org/index.html">vue.js</a> </h3>
+      <img src="@/assets/logo.png"/>
+    </template>
+    <template>
+      <h3>MicroPost</h3>
+      <label for="username"></label>
+      <input id="username" type="text" v-model="username"/>
+      <br><br>
+      <labal for="micropost"></labal>
+      <textarea id="micropost" v-model="micropost"></textarea>
+      <br><br>
+      <button @click="createPost">Post</button>
+      <h2>Micropost Feed</h2>
+    </template>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
   export default {
+    data() {
+      return{
+        username: "",
+        micropost: ""
+      }
+    },
     methods: {
       toSignUp() {
         this.$router.push("signup");
+      },
+      createPost(){
+        axios.post("https://firestore.googleapis.com/v1/projects/vuejs-759de/databases/(default)/documents/posts",
+        {
+          fields:{
+            username: {
+              stringValue:this.username
+            },
+            post: {
+              stringValue:this.micropost
+            }
+          }
+        }
+       ).then(response => {
+         console.log(response);
+       })
+       .catch(error => {
+         console.log(error);
+       })
+      this.username = "";
+      this.post = "";
       }
     }
   }
